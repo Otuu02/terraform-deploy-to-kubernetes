c31bfb3d2ae4233c32cfcb4ca7ad220848d6f175
obinna <giftotuuobinna1995@gmail.com>
2025-01-09 18:28:52 +0100

    another dployment

--- 0_provider.tf
+++ 0_provider.tf
@@ -1,29 +1,36 @@
 terraform {
   required_providers {
     google = {
-      source  = "hashicorp/google"
+      source = "hashicorp/google"
       version = "6.13.0"
     }
   }
 }
 
 provider "google" {
-  project     = var.project_id
-  region      = var.region
-  credentials = file(var.credentials_file)
+  credentials = file("gcp-key.json") # This should match the service account key file in the pipeline
+  project     = var.project_id      # Use a variable for flexibility
+  region      = var.region          # Use a variable for region
 }
 
-variable "project_id" {
-  type        = string
-  description = "The GCP project ID"
-}
 
-variable "region" {
-  type        = string
-  description = "The GCP region"
-}
 
-variable "credentials_file" {
-  type        = string
-  description = "Path to the GCP credentials JSON file"
-}
+
+
+
+
+# 1) Create A service account
+# 2) permission needed
+# 1) Compute Admin
+# 2) Kubernetes Engine Admin
+# 3) Kubernetes Engine Default Node Service Account
+# 4) Service Account Admin
+# 5) Service Account User
+
+
+#  Enable some api
+# ===================
+
+# compute engine api
+# Kubernetes Engine api 
+# Resource manager api
